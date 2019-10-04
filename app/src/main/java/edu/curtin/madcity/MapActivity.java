ebfa2ff0639873daecd4257561dcce75e2aa5d30
benbowers72 <ben.bowers720@outlook.com>
2019-10-04 14:45:16 +0800

    Made vars private

--- app/src/main/java/edu/curtin/madcity/MapActivity.java
+++ app/src/main/java/edu/curtin/madcity/MapActivity.java
@@ -14,9 +14,6 @@ public class MapActivity extends AppCompatActivity
 {
     private static final String TAG = "MapActivity";
 
-    StatusBar mStatusBar;
-    MapGrid mMapGrid;
-    SelectorFragment mSelectorFragment;
 
     public static Intent newIntent(Context packageContext)
     {
@@ -28,43 +25,43 @@ public class MapActivity extends AppCompatActivity
     {
         Log.d(TAG, "onCreate() called");
 
+        StatusBar statusBar;
+        MapGrid mapGrid;
+        SelectorFragment selectorFragment;
+        FragmentManager fm = getSupportFragmentManager();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        FragmentManager fm = getSupportFragmentManager();
+        statusBar = (StatusBar)fm.findFragmentById(R.id.map_status_bar);
 
-
-        mStatusBar = (StatusBar)fm.findFragmentById(R.id.map_status_bar);
-
-        if (mStatusBar == null)
+        if (statusBar == null)
         {
             Log.d(TAG, "creating StatusBar fragment");
 
-            mStatusBar = new StatusBar();
+            statusBar = new StatusBar();
             fm.beginTransaction()
-                    .add(R.id.map_status_bar, mStatusBar).commit();
+                    .add(R.id.map_status_bar, statusBar).commit();
         }
 
-        mMapGrid = (MapGrid)fm.findFragmentById(R.id.map_grid);
+        mapGrid = (MapGrid)fm.findFragmentById(R.id.map_grid);
 
-        if (mMapGrid == null)
+        if (mapGrid == null)
         {
             Log.d(TAG, "creating MapGrid fragment");
 
-            mMapGrid =  new MapGrid();
-            fm.beginTransaction().add(R.id.map_grid, mMapGrid).commit();
+            mapGrid =  new MapGrid();
+            fm.beginTransaction().add(R.id.map_grid, mapGrid).commit();
         }
 
-        mSelectorFragment  =
+        selectorFragment =
                 (SelectorFragment)fm.findFragmentById(R.id.map_selector);
-        if(mSelectorFragment == null)
+        if(selectorFragment == null)
         {
             Log.d(TAG, "creating Selector fragment");
-            mSelectorFragment = new SelectorFragment();
+            selectorFragment = new SelectorFragment();
             fm.beginTransaction()
-                    .add(R.id.map_selector, mSelectorFragment).commit();
+                    .add(R.id.map_selector, selectorFragment).commit();
         }
-
-
     }
 }
